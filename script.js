7e900e6eb39539527f0b8b52c86a1acd6cc8fc3d
Tatev84 <tatevh84@gmail.com>
2021-08-09 00:36:37 +0400

    homework 16

---
+++ script.js
@@ -0,0 +1,55 @@
+/* 
+let date=new Date()
+let mn=date.getMonth()+1
+let m= mn<10? `0${mn}`:mn
+let d= date.getDate()<10? `0${date.getDate()}`:date.getDate()
+
+
+console.log(date.getFullYear()+"/"+m +"/"+d) */
+
+
+
+/* 
+  function birthday(dt){
+     let date=new Date()
+    let year=date.getFullYear()  
+    return year-dt
+    
+}
+
+console.log(birthday(1984,11,27))    */
+
+
+/*function f (date1){
+    var date=new Date(date1)
+    var n=date.getDay()
+    if( n==6|| n==0){
+         console.log("weekend")
+
+         }else{
+             console.log("working day")
+
+         }
+}
+ f("Aug 7, 2021")  */
+
+/*  function f(){
+     var date=new Date()
+     var hours=date.getHours()
+     if(hours<=12){
+         console.log("am")
+
+     }else{
+         console.log("pm")
+     }
+
+ }
+
+ f(new Date)
+  */
+
+/* 
+function f(m){
+    
+var date=new Date()
+var month=date.getMonth()*/
